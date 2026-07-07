9282de1862f30bd5a324cf4ba833d7c46e77d05c
Thiruna0704 <thirunavukarasum.ai2023@dce.edu.in>
2026-07-07 09:36:13 +0530

    Time: 4402 ms (14.75%) | Memory: 12.7 MB (45.18%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        s1 = sorted(s1)
+        k = len(s1)
+
+        for i in range(len(s2) - k + 1):
+            if sorted(s2[i:i+k]) == s1:
+                return True
+
+        return False
+       
